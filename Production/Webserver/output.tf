a8d7d629290546e01b1dd0faf9e764c08a340bfb
rselvaraj15 <priyaselvaraj780@gmail.com>
2023-12-12 18:36:55 +0000

    added output.tf file

--- Production/Webserver/output.tf
+++ Production/Webserver/output.tf
@@ -1,6 +1,6 @@
-# output "public_ip" {
-#   value = aws_instance.acs73026.public_ip
-# }
+ output "public_ip" {
+  value = aws_instance.acs73026.public_ip
+ }
 
 # output "web_eip" {
 #   value = aws_eip.static_eip.public_ip
