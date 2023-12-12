0b77aac5454785cb5460965ecb8c9ccc826de84b
Prason <pgiri5@myseneca.ca>
2023-12-12 19:59:55 +0000

    Final Network For staging.

--- Production/Network/config.tf
+++ Production/Network/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "group1.1-project"                // Bucket where to SAVE Terraform State
+    bucket = "group1.1-project"               // Bucket where to SAVE Terraform State
     key    = "prod/network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
-    region = "us-east-1"                           // Region where bucket is created
+    region = "us-east-1"                      // Region where bucket is created
   }
 }
