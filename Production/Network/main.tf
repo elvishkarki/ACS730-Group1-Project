1a7983df1c9888c245b5026c0f57892511a36954
Prason <pgiri5@myseneca.ca>
2023-12-12 19:16:53 +0000

    Production/Webserver/config and output updated

--- Production/Network/main.tf
+++ Production/Network/main.tf
@@ -1,12 +1,11 @@
 
-# Module to deploy basic networking 
-module "vpc-prod" {
-  source = "../../modules/aws_network"
-  #source             = "github.com/mohammad-seneca/lab4.git"
-  env                = var.env
-  vpc_cidr           = var.vpc_cidr
-  public_cidr_blocks = var.public_subnet_cidrs
+# Module to deploy basic networking for nonProd(development) environment
+module "vpc-nonProd" {
+  source              = "../../../Group1-ACS730-FinalProject/modules/aws_network"
+  env                 = var.env
+  vpc_cidr            = var.vpc_cidr
+  public_cidr_blocks  = var.public_subnet_cidrs
   private_cidr_blocks = var.private_subnet_cidrs
-  prefix             = var.prefix
-  default_tags       = var.default_tags
+  prefix              = var.prefix
+  default_tags        = var.default_tags
 }
--- Production/Network/output.tf
+++ Production/Network/output.tf
@@ -1,12 +1,10 @@
+#Outputs to show vpc, public and private subnet IDs for our network.
 output "public_subnet_ids" {
-  value = module.vpc-prod.subnet_id
+  value = module.vpc-Prod.public_subnet_id
 }
-
-output "vpc_id" {
-  value = module.vpc-prod.vpc_id
-}
-
-
 output "private_subnet_ids" {
-  value = module.vpc-prod.private_subnet_id
+  value = module.vpc-Prod.private_subnet_id
+}
+output "vpc_id" {
+  value = module.vpc-Prod.vpc_id
 }
