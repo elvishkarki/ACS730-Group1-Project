d87550ee43da49e3f5e0a30409328f7c39d54ef8
Prason <pgiri5@myseneca.ca>
2023-12-14 00:56:23 +0000

    Final Commit 2.0

--- development/Network/config.tf
+++ development/Network/config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "group1-project"               // Bucket where to SAVE Terraform State
+    bucket = "group1-project-PS"               // Bucket where to SAVE Terraform State
     key    = "prod/network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
     region = "us-east-1"                      // Region where bucket is created
   }
